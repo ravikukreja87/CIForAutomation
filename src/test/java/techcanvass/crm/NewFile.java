bf0220dfe00f102fde27bc6b54e0282dd6dca54d
Ravi Kukreja <ravi8737@gmail.com>
2018-08-17 00:04:39 +0530

    Adding New File

---
+++ src/test/java/techcanvass/crm/NewFile.java
@@ -0,0 +1,9 @@
+package techcanvass.crm;
+
+public class NewFile {
+
+	public void testMethod() {
+		System.out.println("This is a test method!!!");
+	}
+
+}
--- src/test/java/techcanvass/crm/Test.java
+++ src/test/java/techcanvass/crm/Test.java
@@ -2,8 +2,5 @@ package techcanvass.crm;
 
 public class Test {
 
-	public void testMethod() {
-		System.out.println("This is a test method!!!");
-	}
-
+	
 }
